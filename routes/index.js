9792db9137d568ffcb8f7a6b3c2634b8df9993d8
Chandima Samaraweera <cssam1@hotmail.com>
2017-11-22 21:03:26 -0800

    change db config file for collegedb app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get("/", function(req, res, err) {
     //res.send("INDEX Page");
-    res.render("index", {title: "College API"})
+    res.render("index", {title: "Population API"})
 });
 
 module.exports = router;
